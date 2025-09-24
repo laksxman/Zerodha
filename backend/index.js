e268e5b2c5113e6a4634e9bac00474c9ebdf6249
Lakshman Kumar <kumarlaksxman17@gmail.com>
2025-09-24 20:47:47 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -82,7 +82,7 @@ const app = express();
 
 // Middleware
 app.use(cors());
-const cors = require("cors");
+// const cors = require("cors");
 app.use(cors({
   origin: "https://myfrontend.onrender.com", // your deployed frontend
 }));
